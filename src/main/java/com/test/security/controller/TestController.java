ae4cb73503f45a9984373bcef317cc7ff6865e77
金琦哲 <jqz248977253>
2022-12-12 14:54:06 +0800

    init

---
+++ src/main/java/com/test/security/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.test.security.controller;
+
+import com.test.security.entity.SysUser;
+import com.test.security.service.ISysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author jin
+ * @Date 2022/12/7 14:12
+ * @Description TODO
+ */
+@Controller
+public class TestController {
+
+    @Autowired
+    private ISysUserService iSysUserService;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        SysUser admin = iSysUserService.getUserByName("admin");
+        if (admin!=null){
+            return admin.getUserName();
+        }else{
+            return "test";
+        }
+    }
+
+    @GetMapping("/test1")
+    @ResponseBody
+    public String test11(){
+        return "test1";
+    }
+}
